b1a5b9fb54a3a868739be1e12395b18db9752996
Koenigseder <kevin.koenigseder@outlook.com>
2024-10-23 15:40:17 +0200

    Enhance myName.c

--- gdp/MyName/myName.c
+++ gdp/MyName/myName.c
@@ -1,4 +1,14 @@
+#include<stdio.h>
+
 int main() {
+  int age;
+  char* name;
+
+  age = 22;
+  name = "Kevin Koenigseder";
+
+  printf("Hallo mein Name ist %s\nIch bin %d Jahre alt!\n", name, age);
+
   return 0;
 }
 
