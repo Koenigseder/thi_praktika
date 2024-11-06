83819f44dbf5aba293fd9851204dd3963997300b
Koenigseder <kevin.koenigseder@outlook.com>
2024-11-06 14:16:26 +0100

    Optimize calculation

--- gdp/Worm000/worm.c
+++ gdp/Worm000/worm.c
@@ -69,7 +69,7 @@ int main(void) {
     } else {
         // Center output
         int mid_row = LINES / 2;
-        int start_col = COLS / 2 - msg_len / 2;
+        int start_col = (COLS - msg_len) / 2;
 
         // Write letter A to the top left  corner of our display
         move(0, 0); // Move to position
